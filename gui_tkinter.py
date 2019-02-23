7e782ea2edc5b8fefe7b471c95ae0a51d6dd7ac6
墨大宝 <983248128@qq.com>
2019-02-23 15:55:04 +0800

    改进逻辑与界面
    第一次用GitHub Desktop

---
+++ gui_tkinter.py
@@ -0,0 +1,117 @@
+# coding: utf-8
+"""
+ChinglishCopier's GUI powered by tkinter
+未实现：
+- 监听剪贴板
+
+@author: Mo Dabao
+"""
+
+
+import tkinter as tk
+from organize import organize as organize_core
+
+
+# 帮助文本
+HELP = \
+"""\n\n\n
+清空：
+    清空此文本框的内容。\n\n\n
+整理：
+    若文本框内有内容，则整理文本框内容；
+    若文本框内无内容，则整理剪贴板内容。\n\n\n
+剪切：
+    将文本框内容剪切到剪贴板。
+
+"""
+
+# 关于文本
+ABOUT = \
+"""\n\n\n
+此软件对查重结果概不负责！！！
+\n\n\n
+所有整理的文献内容归原作者所有
+\n\n\n
+==================================================\n\n
+                                      作者：墨大宝\n
+                                微信公众号：碎积云\n
+                                         2019-2-22
+"""
+
+
+class MainWindow(tk.Tk):
+    """
+    主窗口
+    """
+    def __init__(self):
+        super().__init__()
+        self.resizable(width=False, height=False)
+        # 主窗口标题
+        self.title("查重100%！")
+        # 菜单栏
+        self.menubar = tk.Menu(self)
+        self.menubar.add_command(label="帮助", command=self.help)
+        self.menubar.add_command(label="关于", command=self.about)
+        self.config(menu=self.menubar)
+
+        # 文本框
+        self.text_disp = tk.Text(self, width=50, height=30)
+        self.text_disp.insert(tk.INSERT, HELP)
+        self.text_disp.grid(row=0, column=0, columnspan=3)
+        # “清空”按钮
+        self.button_clear = tk.Button(self, text="清空", command=self.clear)
+        self.button_clear.grid(row=1, column=0, sticky=tk.W+tk.E,)
+        # “整理”按钮
+        self.button_oganize = tk.Button(self, text="整理", command=self.organize)
+        self.button_oganize.grid(row=1, column=1, sticky=tk.W+tk.E)
+        # “剪切”按钮
+        self.button_cut = tk.Button(self, text="剪切", command=self.cut)
+        self.button_cut.grid(row=1, column=2, sticky=tk.W+tk.E)
+        # 事件循环
+        self.mainloop()
+
+    def clear(self):
+        """
+        清空text_disp文本框
+        """
+        self.text_disp.delete(0.0, tk.END)  # 清空text_disp文本框内容
+
+    def organize(self):
+        """
+        获得text_disp文本框的内容并整理
+        整理完覆盖掉text_disp文本框的内容
+        """
+        text_old = self.text_disp.get(0.0, tk.END)  # 获取text_disp文本框内容
+        text_old = text_old if not text_old.isspace() else self.clipboard_get()
+        self.clear()
+        text_new = organize_core(text_old)
+        self.text_disp.insert(tk.INSERT, text_new)
+
+    def cut(self):
+        """
+        将文本框内的内容剪切到剪切板
+        """
+        text = self.text_disp.get(0.0, tk.END)
+        self.clear()
+        # 更新剪贴板内容为指定字符串
+        self.clipboard_clear()  # 清除剪贴板内容
+        self.clipboard_append(text)  # 向剪贴板追加内容
+
+    def help(self):
+        """
+        菜单栏“帮助”
+        """
+        self.clear()
+        self.text_disp.insert(tk.INSERT, HELP)
+
+    def about(self):
+        """
+        菜单栏“关于”
+        """
+        self.clear()
+        self.text_disp.insert(tk.INSERT, ABOUT)
+
+
+if __name__ == "__main__":
+    root = MainWindow()
+
---
+++ organize.py
@@ -0,0 +1,84 @@
+# coding: utf-8
+"""
+为了解决从pdf等格式的中文文献中复制粘贴常会出现的一些问题：
+- 数字、字母等非中文字符变成全角字符
+- 产生多余的空格、换行
+- 中、英文标点（全、半角）混乱
+- 英文上下文中的标点后缺空格
+未解决：
+- 括号等依赖上下文判断全、半角的标点未识别
+- 仅支持中英文文献
+
+@author: Mo Dabao
+"""
+
+
+# 可能出现在换行符前的字符
+ENDSIGN = set(".:;!?。：；！？")
+
+# unicode65281～65374中可能要保留的全角标点
+FULLPUNCTUATION = set("，。：；！？～")
+
+# 可能出现在半角空格和标点前的字符
+HALFSIGN = set("abcdefghijklmnopqrstuvwxyz"
+               "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+               "0123456789"
+               ",.:;!?\"')]}")
+
+
+def organize(text_old):
+    """整理从文献中复制出来的文本。
+
+    - 删除多余的空白符（换行、空格、制表等）
+    - 英文、数字、部分标点符号的全角字符换成半角字符
+    - 修正误判为中文标点（，；：！？）的英文标点字符
+    - 英文标点后确保一个空格
+    注意：所有括号都处理为半角
+
+    Args:
+        text_old: 从文献中复制出来的文本。
+
+    Returns:
+        text_new: 整理之后的文本。
+    """
+    text_new = []
+    for char in text_old:
+        char_unicode = ord(char)
+        # 处理换行符
+        if char == '\n':
+            # 若上一个字符为可能出现在换行符前的字符则保留
+            if text_new and text_new[-1] in ENDSIGN:
+                text_new.append(char)
+        # 处理非换行符的空白字符
+        elif char.isspace():
+            # 若上一个字符为可能出现在半角空格前的字符则换成半角空格
+            if text_new and text_new[-1] in HALFSIGN:
+                text_new.append(' ')
+        # 处理unicode65281～65374全角字符
+        elif 65281 <= char_unicode <= 65374:
+            # 若为中文全角标点
+            if char in FULLPUNCTUATION:
+                # 若上一个字符为可能出现在半角标点前的字符则换成半角并加空格
+                if text_new and text_new[-1] in HALFSIGN:
+                    text_new.append(chr(char_unicode - 65248))
+                    text_new.append(' ')
+                # 否则保留
+                else:
+                    text_new.append(char)
+            # 否则换成半角
+            else:
+                text_new.append(chr(char_unicode - 65248))
+        # 处理剩余全角字符均保留
+        elif char not in HALFSIGN:
+            # 若上一个字符是半角空格则删去空格
+            if text_new and text_new[-1] == ' ':
+                del text_new[-1]
+                # 若上一个字符是半角标点则换成全角
+                if text_new[-1] in ",:;?!":
+                    text_new[-1] = chr(ord(text_new[-2]) + 65248)
+            text_new.append(char)
+        # 处理剩余字符直接保留
+        else:
+            text_new.append(char)
+    text_new = ''.join(text_new)
+    return text_new
